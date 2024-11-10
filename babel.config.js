22b93c16ff1bae8a5da85b3267de3f77eeebf300
Walter Ayala <ayawal22@gmail.com>
2024-11-10 12:21:52 -0600

    chore: organize project structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.ts',
+          '.tsx',
+          '.json',
+          '.d.ts',
+          '.svg',
+          '.native.svg',
+          '.android.svg',
+        ],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@domain': './src/domain',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };

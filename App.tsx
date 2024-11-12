4c7f1f63c3c1ad7621571342f441f6e12e0d1ad2
Walter Ayala <ayawal22@gmail.com>
2024-11-12 11:50:43 -0600

    feat: add splash

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import RootStack from '@navigation/RootStack';
 import {NavigationContainer} from '@react-navigation/native';
-import { toastConfig } from '@utils/';
+import {toastConfig} from '@utils/';
 import React, {useRef} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
+import RNBootSplash from 'react-native-bootsplash';
 
 function App(): React.JSX.Element {
   const navigationRef = useRef(null);
+  const onReady = () => {
+    RNBootSplash.hide();
+  };
 
   return (
     <SafeAreaProvider>
-      <NavigationContainer ref={navigationRef}>
+      <NavigationContainer ref={navigationRef} onReady={onReady}>
         <RootStack />
       </NavigationContainer>
       <Toast
